30796f11f072a94d0b5cdcffd898585a4795a03a
Toby Parsons <toby.d.parsons@gmail.com>
2024-05-14 23:27:26 +0100

    Basic refactor for readability

--- script.rb
+++ script.rb
@@ -4,14 +4,15 @@ def bubble_sort(arr)
     for a in arr do
         b = 0
         until b == (arr.length - 1) do
-            def swap(first, val, arr)
-                arr[first], arr[val] = arr[val], arr[first]
-            end
             arr[b] < arr[b + 1] ? true : swap(b, b + 1, arr)
             b += 1
         end
     end
     arr
+end
+
+def swap(first, val, arr)
+    arr[first], arr[val] = arr[val], arr[first]
 end
 
 bubble_sort([4,3,78,2,0,2])
