1f00a57787f0738ade4e8150a15490d5d078d199
Toby Parsons <toby.d.parsons@gmail.com>
2024-05-14 23:18:10 +0100

    Implement bubble_sort method for array sorting
    Introduces the bubble_sort method, which sorts an array by repeatedly comparing adjacent elements and swapping them if they are in the wrong order.
    The process continues until the array is fully sorted.
    his implementation efficiently 'bubbles' the largest elements towards the end of the array on each iteration.

--- script.rb
+++ script.rb
@@ -1,1 +1,18 @@
+require 'pry-byebug'
 
+def bubble_sort(arr)
+    for a in arr do
+        p arr
+        b = 0
+        until b == 5 do
+            def swap(first, val, arr)
+                arr[first], arr[val] = arr[val], arr[first]
+            end
+            arr[b] < arr[b + 1] ? true : swap(b, b + 1, arr)
+            b += 1
+        end
+    end
+    arr
+end
+
+bubble_sort([4,3,78,2,0,2])
